7443f2efc5dc73b60f7a1e60011d49f08a3fcc1f
EYMA18 <124107235+EYMA18@users.noreply.github.com>
2023-02-22 11:25:13 -0600

    PaginaWeb SYSWEB UNACH

---
+++ web/index.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask (__name__)
+
+@app.route('/')
+def index_():
+    return render_template('index.html')
+
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+if __name__== '__main__':
+    app.run(port=4000, host='0.0.0.0')
